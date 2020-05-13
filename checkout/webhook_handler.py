a13aaebbdad1766704d9d91fab9b48f93331a4b6
jonathanw82 <jonwhewaycode@outlook.com>
2020-05-13 18:50:56 +0000

    setup webhook listeners for stripe and have recieved a success message

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -15,3 +15,18 @@ class StripeWH_Handler:
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the succeeded webhook from stripe
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the succeeded webhook from stripe
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
