4bf52a3244b0caf656fa9460a33599407da83a17
jonathanw82 <jonwhewaycode@outlook.com>
2020-05-21 17:29:40 +0000

    test emaila again

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -22,7 +22,7 @@ class StripeWH_Handler:
             'checkout/confirm_email/confirmation_email_subject.txt',
             {'order': order})
         body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email.txt',
+            'checkout/confirm_email/confirmation_email.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
         send_mail(
